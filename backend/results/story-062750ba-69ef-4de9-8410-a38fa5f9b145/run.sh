930df917474fac11dc6e62f0a13ce090533c395f
principia-ai <principia.ai@outlook.com>
2025-03-14 18:18:36 +0300

    Remove backend/results from git tracking

--- backend/results/story-062750ba-69ef-4de9-8410-a38fa5f9b145/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/recursive
-source /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-062750ba-69ef-4de9-8410-a38fa5f9b145/api_key.env
-python engine.py --filename /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-062750ba-69ef-4de9-8410-a38fa5f9b145/input.jsonl --output-filename /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-062750ba-69ef-4de9-8410-a38fa5f9b145/result.jsonl --done-flag-file /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-062750ba-69ef-4de9-8410-a38fa5f9b145/done.txt --model gpt-4o --mode story
--- backend/results/story-343c1f4e-4704-4cca-9ff7-cb16396df4c4/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/recursive
-source /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-343c1f4e-4704-4cca-9ff7-cb16396df4c4/api_key.env
-python engine.py --filename /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-343c1f4e-4704-4cca-9ff7-cb16396df4c4/input.jsonl --output-filename /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-343c1f4e-4704-4cca-9ff7-cb16396df4c4/result.jsonl --done-flag-file /Users/cheny0x/Documents/Projects/heterogeneous-recursive-planning/backend/results/story-343c1f4e-4704-4cca-9ff7-cb16396df4c4/done.txt --model claude-3-5-sonnet-20241022 --mode story
